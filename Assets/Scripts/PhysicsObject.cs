00e438c3770567f3465fcc09d24f7b07d5521d98
Kyle Donoghue <kdonoghue2019@gmail.com>
2020-05-26 23:35:57 -0700

    Wall Jump v1

--- Assets/Scripts/PhysicsObject.cs
+++ Assets/Scripts/PhysicsObject.cs
@@ -7,11 +7,16 @@ public class PhysicsObject : MonoBehaviour {
     [Header("Physics Settings")]
     public float minGroundNormalY = .65f;
     public float distanceFromGround = 1f;
+    public float distanceFromWall =  .52f;
     public float gravityModifier = 1f;
+    public float wallGravityModifier = .5f;
+    public float timeSinceWall = .3f;
+
     public bool debugRay;
     
     [Header("Physics Results")] 
     public bool isGrounded;
+    public bool isWalled;
     public float slope;
     public float heightFromGround;
 
@@ -21,11 +26,13 @@ public class PhysicsObject : MonoBehaviour {
     protected Vector2 targetVelocity;
     protected Vector2 groundNormal;
     protected Rigidbody2D rb2d;
-    protected Vector2 velocity;
+    public Vector2 velocity;
     protected ContactFilter2D contactFilter;
     protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
     protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D> (16);
-
+    protected bool wallSide; //false = left
+    protected float wallInitial;
+    protected float wallTime = 0;
     protected const float minMoveDistance = 0.001f;
     protected const float shellRadius = 0.01f;
 
@@ -51,10 +58,19 @@ public class PhysicsObject : MonoBehaviour {
     {
 
     }
-
+    protected virtual void ResistanceX() {
+        
+    }
     void FixedUpdate()
     {
-        if (!onLadder) {
+        ResistanceX();
+       /* if (!onLadder) {
+            velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        }*/
+        if(velocity.y < 0) {
+            velocity += gravityModifier * (isWalled ? wallGravityModifier : 1) * Physics2D.gravity * Time.deltaTime;
+        }
+        else {
             velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
         }
         velocity.x = targetVelocity.x;
@@ -66,20 +82,91 @@ public class PhysicsObject : MonoBehaviour {
         Vector2 moveAlongGround = new Vector2 (groundNormal.y, -groundNormal.x);
 
         Vector2 move = moveAlongGround * deltaPosition.x;
-
         Movement (move, false);
 
         move = Vector2.up * deltaPosition.y;
 
         Movement (move, true);
 
-        Vector3 padding = Vector3.zero;
-        /*if (facing) {
-            padding.x = size.x/2;
+        rayCastGrounded();
+        rayCastWalls();
+
+        
+
+    }
+
+    void Movement(Vector2 move, bool yMovement)
+    {
+        float distance = move.magnitude;
+
+        if (distance > minMoveDistance) 
+        {
+            int count = rb2d.Cast (move, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear ();
+            for (int i = 0; i < count; i++) {
+                hitBufferList.Add (hitBuffer [i]);
+            }
+
+            for (int i = 0; i < hitBufferList.Count; i++) 
+            {
+                Vector2 currentNormal = hitBufferList [i].normal;
+                if (currentNormal.y > minGroundNormalY) 
+                {
+                    if (yMovement) 
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot (velocity, currentNormal);
+                if (projection < 0) 
+                {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList [i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+
+
+        }
+
+        rb2d.position = rb2d.position + move.normalized * distance;
+    }
+
+
+
+    void rayCastWalls() {
+        RaycastHit2D hitL = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.left), Mathf.Infinity, LayerMask.GetMask("Ground"), -Mathf.Infinity, Mathf.Infinity);
+        RaycastHit2D hitR = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.right), Mathf.Infinity, LayerMask.GetMask("Ground"), -Mathf.Infinity, Mathf.Infinity);
+        if (debugRay) {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.left) * hitL.distance, Color.black);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.right) * hitR.distance, Color.black);
+        }
+        if(!isGrounded) {
+            if ((hitL.distance <= distanceFromWall && hitL.distance != 0) || (hitR.distance <= distanceFromWall && hitR.distance != 0)) {
+                isWalled = true;
+                if(hitL.distance <= distanceFromWall && hitL.distance != 0) {
+                    wallSide = false;
+                }
+                if (hitR.distance <= distanceFromWall && hitR.distance != 0) {
+                    wallSide = true;
+                }
+            }
+            else {
+                isWalled = false;
+            }
         }
         else {
-            padding.x = -size.x/2;
-        }*/
+            isWalled = false;
+        }
+    }
+
+
+
+    void rayCastGrounded() {
+        Vector3 padding = Vector3.zero;
         padding.x = size.x/2;
         RaycastHit2D hitL = Physics2D.Raycast(transform.position - padding, transform.TransformDirection(Vector2.down), Mathf.Infinity, LayerMask.GetMask("Ground"), -Mathf.Infinity, Mathf.Infinity);
         RaycastHit2D hitR = Physics2D.Raycast(transform.position + padding, transform.TransformDirection(Vector2.down), Mathf.Infinity, LayerMask.GetMask("Ground"), -Mathf.Infinity, Mathf.Infinity);
@@ -136,84 +223,6 @@ public class PhysicsObject : MonoBehaviour {
                 slope = Mathf.Abs(hitR.normal.x/hitR.normal.y);
             }
         }
-        /*
-        if(hitL.distance == 0 && hitR.distance == 0) {
-            Debug.Log("both");
-            isGrounded = false;
-            distanceFromGround = Mathf.Infinity;
-            slope = 0;
-        }
-        if(hitL.distance < hitR.distance || (hitR.distance == 0 && hitL.distance != 0)) {
-                        Debug.Log("left");
-
-            if (debugRay) {
-                Debug.DrawRay(transform.position - padding, transform.TransformDirection(Vector2.down) * hitL.distance, Color.blue);
-                Debug.DrawRay(transform.position + padding, transform.TransformDirection(Vector2.down) * hitR.distance, Color.black);
-            }
-            heightFromGround = hitL.distance;
-            if (heightFromGround < distanceFromGround) {
-                isGrounded = true;
-                slope = Mathf.Abs(hitL.normal.x/hitL.normal.y);
-            }
-        }
-        else if (((hitR.distance <= hitL.distance) && hitR.distance != 0) || (hitL.distance == 0 && hitR.distance != 0)) {
-                        Debug.Log("right");
-
-            if (debugRay) {
-                Debug.DrawRay(transform.position - padding, transform.TransformDirection(Vector2.down) * hitL.distance, Color.black);
-                Debug.DrawRay(transform.position + padding, transform.TransformDirection(Vector2.down) * hitR.distance, Color.blue);
-            }
-            heightFromGround = hitR.distance;
-            if (heightFromGround < distanceFromGround) {
-                isGrounded = true;
-                slope = Mathf.Abs(hitR.normal.x/hitR.normal.y);
-            }
-        }
-        */
-        
-
-    }
-
-    void Movement(Vector2 move, bool yMovement)
-    {
-        float distance = move.magnitude;
-
-        if (distance > minMoveDistance) 
-        {
-            int count = rb2d.Cast (move, contactFilter, hitBuffer, distance + shellRadius);
-            hitBufferList.Clear ();
-            for (int i = 0; i < count; i++) {
-                hitBufferList.Add (hitBuffer [i]);
-            }
-
-            for (int i = 0; i < hitBufferList.Count; i++) 
-            {
-                Vector2 currentNormal = hitBufferList [i].normal;
-                //Debug.Log(currentNormal);
-                if (currentNormal.y > minGroundNormalY) 
-                {
-                    //grounded = true;
-                    if (yMovement) 
-                    {
-                        groundNormal = currentNormal;
-                        currentNormal.x = 0;
-                    }
-                }
-
-                float projection = Vector2.Dot (velocity, currentNormal);
-                if (projection < 0) 
-                {
-                    velocity = velocity - projection * currentNormal;
-                }
-
-                float modifiedDistance = hitBufferList [i].distance - shellRadius;
-                distance = modifiedDistance < distance ? modifiedDistance : distance;
-            }
-
-
-        }
-
-        rb2d.position = rb2d.position + move.normalized * distance;
     }
 
 }
--- Assets/Scripts/PlayerPlatformerController.cs
+++ Assets/Scripts/PlayerPlatformerController.cs
@@ -12,6 +12,7 @@ public class PlayerPlatformerController : PhysicsObject {
     public float ladderSpeed = 5;
     [Header("Player Results")]
     public float height;
+    public bool affected;
 
     public bool canLadder = false;
     private SpriteRenderer spriteRenderer;
@@ -27,13 +28,65 @@ public class PlayerPlatformerController : PhysicsObject {
         size = box.size+new Vector2(2*radius,2*radius);
         height = box.size.y;
     }
-
+    public void addVelocity(Vector2 newVel, bool overwriteX, bool overwriteY) {
+        Vector2 newVelocity = velocity;
+        if(overwriteX) {
+            newVelocity.x = newVel.x;
+        }
+        else {
+            newVelocity.x += newVel.x;
+        }
+        if(overwriteY) {
+            newVelocity.y = newVel.y;
+        }
+        else {
+            newVelocity.y += newVel.y;
+        }
+        targetVelocity = newVelocity;
+    }
+    void addMoveX(float movex) {
+        if (!affected) {
+            if (velocity.x <= maxSpeed && velocity.x >= -maxSpeed) {
+                addVelocity(new Vector2(maxSpeed * movex,0), true, false);
+                return;
+            }
+        }
+        /*
+        if(movex > 0) {
+            if (velocity.x <= maxSpeed - (2f+.1f) && velocity.x >= -maxSpeed) {
+                if ( velocity.x < 0) {
+                    addVelocity(new Vector2(0,0), true, false);
+                }
+                    addVelocity(new Vector2(2f,0), false, false);
+            }
+            else if (velocity.x > maxSpeed - (2f+.1f) && velocity.x <= maxSpeed) {
+                addVelocity(new Vector2(maxSpeed * movex,0), true, false);
+            }
+            else if (velocity.x < -maxSpeed) {
+                addVelocity(new Vector2(.5f,0), false, false);
+            }
+        }
+        else if (movex < 0) {
+            if (velocity.x <= maxSpeed && velocity.x >= -maxSpeed + (2f+.1f)) {
+                if ( velocity.x > 0) {
+                    addVelocity(new Vector2(0,0), true, false);
+                }
+                addVelocity(new Vector2(-2f,0), false, false);
+            }
+            else if (velocity.x < -maxSpeed + (2f+.1f) && velocity.x >= -maxSpeed) {
+                addVelocity(new Vector2(maxSpeed * movex,0), true, false);
+            }
+            else if (velocity.x > maxSpeed) {
+                addVelocity(new Vector2(-.5f,0), false, false);
+            }
+        }*/
+    }
     protected override void ComputeVelocity()
     {
         Vector2 move = Vector2.zero;
 
-        move.x = Input.GetAxis ("Horizontal");
-        onLadder = canLadder ? onLadder : false;
+        move.x = Input.GetAxis("Horizontal");
+        /*onLadder = canLadder ? onLadder : false;
         if (canLadder) {
             if(onLadder) {
                 velocity.y = Input.GetAxis ("Vertical") * ladderSpeed;
@@ -47,15 +100,51 @@ public class PlayerPlatformerController : PhysicsObject {
                     velocity.y = ladderSpeed;
                 }
             }
-        }
-        if (Input.GetButtonDown ("Jump") && isGrounded && !onLadder) {
+        }*/
+        //targetVelocity = move * maxSpeed;
+        bool wasWalled = (Time.time - wallTime) < timeSinceWall;
+
+
+        if (Input.GetButtonDown ("Jump") && isGrounded /*&& !onLadder*/) {
             velocity.y = jumpTakeOffSpeed;
-        } else if (Input.GetButtonUp ("Jump")) 
-        {
+        }
+        else if (Input.GetButtonDown ("Jump") && isWalled) {
+            wallTime = Time.time;
+            velocity.y = jumpTakeOffSpeed;
+            Vector2 takeOff = Vector2.zero;
+            takeOff.y = move.y;
+            takeOff.x = jumpTakeOffSpeed * 2 * (!wallSide ? 1 : -1);
+            wallInitial = takeOff.x;
+            addVelocity(takeOff, true, true);
+            affected = true;
+        }
+        else if (wasWalled) {
+            Vector2 takeOff = Vector2.zero;
+            takeOff.y = move.y;
+            takeOff.x = wallInitial * (timeSinceWall - (Time.time - wallTime)) / timeSinceWall;
+            if (move.x != 0) {
+                if (move.x > 0 && wallInitial > 0) {
+                    if(takeOff.x < maxSpeed)
+                        takeOff.x = maxSpeed;
+                }
+                else if (move.x < 0 && wallInitial < 0) {
+                    if(takeOff.x > -maxSpeed)
+                        takeOff.x = -maxSpeed;
+                }
+            }
+            addVelocity(takeOff, true, true);
+        }
+        else if (!wasWalled) {
+            affected = false;
+        }
+        else if (Input.GetButtonUp ("Jump")) {
             if (velocity.y > 0) {
-                velocity.y = velocity.y * 0.5f;
+                addVelocity(new Vector2(0,velocity.y * 0.5f), false, true);
             }
         }
+        addMoveX(move.x);
+
+
 
         //bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
         
@@ -76,14 +165,5 @@ public class PlayerPlatformerController : PhysicsObject {
             animator.SetBool ("grounded", isGrounded);
             animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
         }
-        targetVelocity = move * maxSpeed;// * (1 - (1/maxSlope)*(slope));
-        /*if (slope >= maxSlope) {
-            targetVelocity = Vector2.zero;
-        }
-        else {
-            targetVelocity = move * maxSpeed * (1-2*slope*slope);
-        }
-        Debug.Log(targetVelocity);*/
-        //Debug.Log(Mathf.Round(10*(Mathf.Abs(10-  (  -((slope/maxSlope)*12-10)  +  Mathf.Sqrt(  ((slope/maxSlope)*12-10)*((slope/maxSlope)*12-10)  +  4  )  )/2  )))/10);
     }
 }
